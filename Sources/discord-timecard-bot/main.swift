fa7cecf16b1c065033c73fad315e9f0391c5cd0b
rinsuki <428rinsuki+git@gmail.com>
2019-11-19 22:47:28 +0900

    チャンネル移動時のメッセージに.が抜けてた

--- Sources/discord-timecard-bot/main.swift
+++ Sources/discord-timecard-bot/main.swift
@@ -63,7 +63,7 @@ class MyBot: DiscordClientDelegate {
             userJoinedChannelDictionary[voiceState.userId] = currentChannel
             
             if let lastChannel = lastChannel { // move
-                createPost("[\(currentChannel.name)] \(nickname) moved from \(lastChannel.name)")
+                createPost("[\(currentChannel.name)] \(nickname) moved from \(lastChannel.name).")
             } else { // join
                 createPost("[\(currentChannel.name)] \(nickname) joined.")
             }
